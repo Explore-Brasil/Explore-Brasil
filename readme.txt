4f697b9458d030e694f92af71d74b823b7e05c66
Arthur Silva Mendonça <arthu91@gmail.com>
2023-03-07 16:39:18 -0300

    feat: entregando Landing Page versão mobile e desktop

---
+++ readme.txt
@@ -0,0 +1,7 @@
+PUSH
+git push origin landing-page
+
+git checkout develop
+
+ATUALIZAR REPO 
+git pull origin develop
---
+++ src/pages/LandingPage/Title/index.tsx
@@ -0,0 +1,10 @@
+const Title = () => {
+  return (
+    <div className="title__wrapper">
+      <h1>Explore Brasil</h1>
+      <h4>Sua viagem começa aqui</h4>
+    </div>
+  );
+};
+
+export default Title;

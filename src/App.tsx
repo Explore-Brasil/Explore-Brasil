45b58d4c3fbac7dd86c2e071a131a88fc19d054d
Douglas S Werneck <swdoug_@hotmail.com>
2023-03-08 09:32:24 -0300

    feat: hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-<<<<<<< HEAD
-import { HeaderComponent } from './components/Header-Componet/Header-Component'
-import GlobalStyled from './GlobalStyled'
-
-
-
-function App() {
-  
-=======
 import Router from './routes/Router'
 
 function App() {
 
->>>>>>> origin
   return (
-    <>
-    <GlobalStyled/>
     
     <div className="App">
-<<<<<<< HEAD
-      <HeaderComponent/>
-=======
       <Router/>
->>>>>>> origin
     </div>
-    </>
   )
 }
 

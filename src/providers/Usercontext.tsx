7800a2b7ce9d862faeae977bf8c0a64a842b736f
Douglas S Werneck <swdoug_@hotmail.com>
2023-03-08 08:47:21 -0300

    feat: hotfix

--- src/providers/Usercontext.tsx
+++ src/providers/Usercontext.tsx
@@ -73,7 +73,6 @@ export const UserProvider = ({ children }: IUserContextProps) => {
     useEffect(()=> {
         const token = localStorage.getItem('@TOKEN')
         if(!token){
-            navigate('/')
         }
         if(token){
             navigate("/dashboard")
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import RegisterPage from "../pages/RegisterPage";
+import {LoginPage} from "../pages/LoginPage"
 
 const Router = () => (
   <Routes>

e963844d8d0d1a17e9e6c2f40aa5a1188bb27fd3
AdrianoAraujo <araujoadriano95@gmail.com>
2023-03-08 08:19:46 -0300

    feat: loginComponent

--- src/providers/Usercontext.tsx
+++ src/providers/Usercontext.tsx
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom"
 import api from "../services/api";
 
 interface IUserContext {
-    registerUser: (regiterData: IRegisterData) => Promise<void>
+    registerUser: (regiterData: IRegisterData) => Promise<void>;
+    loginUser: (loginData: ILoginUserData) => Promise<void>;
 }
 
 interface IUserContextProps {
@@ -17,6 +18,14 @@ interface IRegisterData {
     passwordConfirmation? : string
 }
 
+export interface ILoginUserData {
+    email: string;
+    password: string;
+}
+
+
+
+
 export const Usercontext = createContext({} as IUserContext)
 
 export const UserProvider = ({ children }: IUserContextProps) => {
@@ -27,16 +36,31 @@ export const UserProvider = ({ children }: IUserContextProps) => {
         delete regiterData.passwordConfirmation
         try {
             await api.post("/users", regiterData)
-            console.log('usuario cadastrado')
+            console.log('usuario cadastrado ~trocar por toast')
             navigate('/register')
         } catch (error) {
-            console.log(error)
+            console.log(error, '~trocar por toast')
+        }
+    }
+
+    const loginUser = async (loginData: ILoginUserData) => {
+        console.log('chamou')
+        try {
+            const response = await api.post('/login', loginData)
+            localStorage.setItem('@TOKEN', response.data.accessToken)
+            localStorage.setItem('@ID', response.data.user.id)
+            navigate('/dashboard')
+            console.log('Logado com sucesso ~trocar por toast')
+            
+        } catch (error) {
+            console.log('Não foi possivel logar-se ~trocar por toast')
+            
         }
     }
 
     return (
         <Usercontext.Provider
-            value={{ registerUser }}
+            value={{ registerUser, loginUser }}
         >
             {children}
         </Usercontext.Provider>
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import { LoginPage } from '../pages/LoginPage';
 import RegisterPage from "../pages/RegisterPage"
 
 const Router = () => (
   <Routes>
     <Route path='/register' element={<RegisterPage />} />
+    <Route path='/login' element={<LoginPage />} />
   </Routes>
 );
 

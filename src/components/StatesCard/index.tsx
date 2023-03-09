ee506dcab3c67f7373914859a5fd120c8f7b35d8
AdrianoAraujo <araujoadriano95@gmail.com>
2023-03-09 15:58:42 -0300

    feat: fixedmodal

--- src/components/StatesCard/index.tsx
+++ src/components/StatesCard/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { StatesContext } from "../../providers/Statescontext";
 import StyledUl from "./styles";
 import ModalCreateComment from "../ModalCreateComments"
@@ -6,17 +6,21 @@ import ModalCreateComment from "../ModalCreateComments"
 const StatesCard = () => {
   const { states } = useContext(StatesContext);
   const {modalIsOpen, setModalIsOpen} = useContext(StatesContext)
+  const [modalState, setModalState] = useState(null)
 
   const findstate = (event) => {
     const stateFound = states.find((state) => state.id === +event.target.id )
+    console.log(stateFound, 'statefound')
     if(stateFound){
       setModalIsOpen(true)
+      return
     } else {
       console.log('ola mundo')
     }
   }
 
   return (
+      <>
     <StyledUl>
       {states.map((state) => (
         <li key={state.id}>
@@ -29,17 +33,22 @@ const StatesCard = () => {
               <p>{state.info}</p>
             </div>
             <div className="button__wrapper">
-              <button id={(state.id).toString()} type="button" onClick={(event)=> findstate(event)}>Adicionar comentário</button>
+              <button id={(state.id).toString()} type="button" onClick={(event)=> {
+                console.log('chamou botão')
+                setModalState(state.id)
+                findstate(event)
+                }}>Adicionar comentário</button>
               <button>Ver informações sobre {state.name}</button>
             </div>
           </div>
-          {
-            modalIsOpen &&
-          <ModalCreateComment stateId={state.id}/>
-          }
+          
         </li>
       ))}
+      {
+            modalIsOpen && <ModalCreateComment stateId={modalState}/>
+          }
     </StyledUl>
+      </>
   );
 };
 
